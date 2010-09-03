d033d2660bcfe2510b206be912095e8ef5ab59fc
simon <simonsunimail@gmail.com>
2010-09-03 11:49:29 +0000

    The register settings for the McBSP ports seem to be nearly correct now, but untested with IRQ/DMA due to that kernel printk bug I need to fix first. Anyway, the data structure containing the settings (tested in polling mode at least) is called "simon_reg".
    I didn't find any reference to clock-stop modes in McBSP ports of OMAP3 anymore (compared to OMAP5912 or C6713 reference manuals and the linux 
    kernel mcbsp.c file). I guess the clock-stop functionality was dropped from McBSP ports in OMAP3 because there are now the McSPI ports to be used instead. 
    Stupid, because I have a devide that wants to be configured in clock-stop mode and then sends framed data, needing an McBSP. Well, I'll just have 
    to actually use the chip select line (CS) of our ADS1258 I guess.
    
    
    
    
    git-svn-id: file:///Users/simon/Workspaces/WorkspaceSVN/ISIP_svn/svn/beagleboard/trunk@9 1a49bced-557f-0410-a62c-c1dc85da7da9

--- mcbsp_ads1258_noDMA_kernelmodule/recipes/helloMcBSP/files/helloMcBSP.c
+++ mcbsp_ads1258_noDMA_kernelmodule/recipes/helloMcBSP/files/helloMcBSP.c
@@ -41,10 +41,11 @@ struct omap_mcbsp_reg_cfg ads1274_cfg = {
 	0x0000, // xcerg;
 	0x0000, //xcerh;
  	0x0000, // xccr;
-	0x0000, //rccr;
-	0x0308 //syscon
+	0x0000 //rccr;
+	//0x0308 //syscon
 };
 
+/*original from http://old.nabble.com/ADC--McBSP-advice-and-questions-about-writing-a-driver.-td26889185.html */
 static struct omap_mcbsp_reg_cfg mcbsp_regs = {
         .spcr2 = FREE,
         .spcr1 = RINTM(0),
@@ -58,15 +59,49 @@ static struct omap_mcbsp_reg_cfg mcbsp_regs = {
         //.pcr0 = CLKXP | CLKRP,        /* mcbsp: slave */
 };
 
+static struct omap_mcbsp_reg_cfg simon_regs = {
+        .spcr2 = XINTM(0),
+        .spcr1 = RINTM(0),
+        .rcr2  = 0,
+        .rcr1  = RFRLEN1(0) | RWDLEN1(OMAP_MCBSP_WORD_32),  // frame is 1 word. word is 32 bits.
+        .xcr2  = 0,
+        .xcr1  = XFRLEN1(0) | XWDLEN1(OMAP_MCBSP_WORD_32),
+        .srgr1 = FWID(31) | CLKGDV(50),
+        .srgr2 = 0 | /*CLKSM |*/ CLKSP  | FPER(255),// | FSGM, // see pages 129 to 131 of sprufd1.pdf
+        .pcr0  = FSXM | CLKXM | CLKRM | FSRM | FSXP | FSRP | CLKXP | CLKRP,
+        //.pcr0 = CLKXP | CLKRP,        /* mcbsp: slave */
+	//.xccr = XDISABLE,
+};
+
+static struct omap_mcbsp_reg_cfg spimode_regs = {
+        .spcr2 = 0,
+        .spcr1 = RINTM(0),
+        .rcr2  = RFRLEN2(OMAP_MCBSP_WORD_16) | RWDLEN2(OMAP_MCBSP_WORD_16) | RDATDLY(1),
+        .rcr1  = RFRLEN1(OMAP_MCBSP_WORD_16) | RWDLEN1(OMAP_MCBSP_WORD_16),
+        .xcr2  = XFRLEN2(OMAP_MCBSP_WORD_16) | XWDLEN2(OMAP_MCBSP_WORD_16) | XDATDLY(1),
+        .xcr1  = XFRLEN1(OMAP_MCBSP_WORD_16) | XWDLEN1(OMAP_MCBSP_WORD_16),
+        .srgr1 = FWID(15) | CLKGDV(50),
+        .srgr2 = 0 | /*CLKSM |*/ CLKSP  | FPER(255),// | FSGM, // see pages 129 to 131 of sprufd1.pdf
+        .pcr0  = FSXM | CLKXM | CLKRM | FSRM | FSXP | FSRP | CLKXP | CLKRP,
+        //.pcr0 = CLKXP | CLKRP,        /* mcbsp: slave */
+	//.xccr = XDISABLE,
+};
+
 const struct omap_mcbsp_spi_cfg ads1274_cfg_spi = {
 	OMAP_MCBSP_SPI_MASTER,
 	OMAP_MCBSP_CLK_RISING,
 	OMAP_MCBSP_CLK_RISING,
 	OMAP_MCBSP_FS_ACTIVE_LOW,
 	16,
-	OMAP_MCBSP_CLK_STP_MODE_NO_DELAY,
+	OMAP_MCBSP_CLK_STP_MODE_DELAY,
 	OMAP_MCBSP_WORD_32
 };
+
+int omap_mcbsp_read_simon(void __iomem *io_base, u16 reg)
+{
+	printk(KERN_ALERT "reading four bytes from address!\n");
+	return __raw_readl(io_base + reg);
+}
 
 int hello_init(void)
 {
@@ -74,34 +109,60 @@ int hello_init(void)
 	int reqstatus = -4;
 	int value = 0;
 	int returnstatus = 0;
-	u32 mybuffer = 0xABCD;
-	
+	u32 mybuffer = 0x5CCC333A; // 01011100110011000011001100111010
+	u32 mybuffer2 = 0x53CA; // 0101 00111100 1010
+
 
 	printk(KERN_ALERT "Hello McBSP world!\n");
 
 	/* Setting IO type & requesting McBSP */
-	status = omap_mcbsp_set_io_type(mcbspID, OMAP_MCBSP_POLL_IO);
+	status = omap_mcbsp_set_io_type(mcbspID, OMAP_MCBSP_IRQ_IO);
+	
 	reqstatus = omap_mcbsp_request(mcbspID);
 	printk(KERN_ALERT "Setting IO type was %d. Requesting McBSP %d returned: %d \n", status, (mcbspID+1), reqstatus);
 
 	if (!reqstatus)
 	{
-	/* configure McBSP */
-	omap_mcbsp_set_spi_mode(mcbspID, &ads1274_cfg_spi);
-	printk(KERN_ALERT "Configured McBSP %d for SPI mode.\n", (mcbspID+1));
-	//omap_mcbsp_config(mcbspID, &mcbsp_regs);
-	//printk(KERN_ALERT "Configured McBSP %d registers..\n", (mcbspID+1));
+		//status = omap_mcbsp_set_io_type(mcbspID, OMAP_MCBSP_POLL_IO);
 
-	/* start McBSP */
-	omap_mcbsp_start(mcbspID);
-	printk(KERN_ALERT "Started McBSP %d. \n", (mcbspID+1));
+		/* configure McBSP */
+		//omap_mcbsp_set_spi_mode(mcbspID, &ads1274_cfg_spi);
+		//printk(KERN_ALERT "Configured McBSP %d for SPI mode.\n", (mcbspID+1));
+		omap_mcbsp_config(mcbspID, &simon_regs);
+		printk(KERN_ALERT "Configured McBSP %d registers for raw mode..\n", (mcbspID+1));
 
-	/* polled mcbsp i/o operations */
-	//status = omap_mcbsp_spi_master_xmit_word_poll(mcbspID, mybuffer);
-	//printk(KERN_ALERT "Writing to McBSP %d in SPI mode returned as status: %d \n", (mcbspID+1), status);
-	status = omap_mcbsp_pollwrite(mcbspID, mybuffer);  // needs changes in mcbsp.c --> kernel patch & recompile
-	////omap_mcbsp_xmit_word(mcbspID, mybuffer); // currently produces segfault, probably because nothing dma-like has been set up yet?
-	printk(KERN_ALERT "Writing to McBSP %d returned as status: %d \n", (mcbspID+1), status);
+		/* start McBSP */
+		omap_mcbsp_start(mcbspID);
+		printk(KERN_ALERT "Started McBSP %d. \n", (mcbspID+1));
+
+		//value = reg32r(OMAP34XX_MCBSP1_BASE, OMAP_MCBSP_REG_SPCR2);
+		//printk(KERN_ALERT "Value of McBSP register SPCR2: %d \n", value);
+	
+		/* show McBSP register contents: */
+		//printk(KERN_ALERT "MCBSP1_SRGR2: 0x%04x\n", __raw_readl( (*(OMAP34XX_MCBSP1_BASE + OMAP_MCBSP_REG_SRGR2)) ));
+
+		/* polled SPI mode operations */
+		printk(KERN_ALERT "Now reading data from McBSP %d in SPI mode... \n", (mcbspID+1));
+		status = omap_mcbsp_spi_master_recv_word_poll(mcbspID, &value);
+		printk(KERN_ALERT "Reading from McBSP %d in SPI mode returned as status: %d and as value: 0x%04x \n", (mcbspID+1), status,value);
+		printk(KERN_ALERT "Now writing data to McBSP %d in SPI mode... \n", (mcbspID+1));
+		status = omap_mcbsp_spi_master_xmit_word_poll(mcbspID, mybuffer);
+		printk(KERN_ALERT "Writing to McBSP %d in SPI mode returned as status: %d \n", (mcbspID+1), status);
+
+		/* polled mcbsp i/o operations */
+		printk(KERN_ALERT "Now writing data to McBSP (raw & polled) %d... \n", (mcbspID+1));
+		status = omap_mcbsp_pollwrite(mcbspID, mybuffer);  // needs changes in mcbsp.c --> kernel patch & recompile
+		printk(KERN_ALERT "Writing to McBSP %d (raw, polled mode) returned as status: %d \n", (mcbspID+1), status);
+		printk(KERN_ALERT "Now writing 2nd data to McBSP (raw & polled) %d... \n", (mcbspID+1));
+		status = omap_mcbsp_pollwrite(mcbspID, mybuffer2);  // needs changes in mcbsp.c --> kernel patch & recompile
+		printk(KERN_ALERT "Writing to McBSP %d (raw, polled mode) returned as status: %d \n", (mcbspID+1), status);
+		printk(KERN_ALERT "Now reading data from McBSP (raw & polled) %d... \n", (mcbspID+1));
+		status = omap_mcbsp_pollread(mcbspID, &mybuffer2);  // needs changes in mcbsp.c --> kernel patch & recompile
+		printk(KERN_ALERT "Reading from McBSP %d (raw, polled mode) returned as status: %d and as value %d \n", (mcbspID+1), status,mybuffer2);
+
+		/* Non-Polled operations (IRQ or DMA!) */
+		omap_mcbsp_xmit_word(mcbspID, mybuffer); // currently produces segfault, probably because nothing dma-like has been set up yet? No, the segfault was because i was not using OMAP_MCBSP_IRQ_IO in omap_mcbsp_set_io_type(). Still, it now either fails on start (when taking mcbsp out of reset, sprc2_XINTM=0) or on dma/irq transfer (sprc2_XINTM=1)
+		printk(KERN_ALERT "Writing to McBSP %d via IRQ or DMA! Return status: %d \n", (mcbspID+1), status);
 	}
 	else printk(KERN_ALERT "Not attempting to continue because requesting failed.\n");
 
@@ -112,6 +173,8 @@ int hello_init(void)
 
 void hello_exit(void)
 {	
+	printk(KERN_ALERT "Stopping McBSP %d...", (mcbspID+1));
+	omap_mcbsp_stop(mcbspID);
 	printk(KERN_ALERT "Freeing McBSP %d...", (mcbspID+1));
 	omap_mcbsp_free(mcbspID);
 	printk(KERN_ALERT "done.\n");
