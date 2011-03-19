9c43419f5eb38170c627e76c4ee16a4167ab0929
fink <fink>
2011-03-19 14:44:28 +0000

    raw mcbsp-acces proved working! :)
    git-svn-id: file:///Users/simon/Workspaces/WorkspaceSVN/ISIP_svn/svn/beagleboard/trunk@56 1a49bced-557f-0410-a62c-c1dc85da7da9

--- mcbsp_test_code_michael/files/kmodule_test.c
+++ mcbsp_test_code_michael/files/kmodule_test.c
@@ -121,9 +121,9 @@ int init_module(void)
 		my_mcbsp_confic.mcr2=0x0;  //multichan-register 
 		my_mcbsp_confic.mcr1=0x0;
 		
-		my_mcbsp_confic.pcr0 =  FSXM|CLKXM|FSXP|FSRP|CLKRP;//pin control register
+		my_mcbsp_confic.pcr0 =  FSXM|CLKXM|FSXP|FSRP|CLKRP|CLKXP;//pin control register
 		
-		my_mcbsp_confic.rcerc=0;//multichan-options... ignore? 
+		my_mcbsp_confic.rcerc=0; //multichan-options... ignore? 
 		my_mcbsp_confic.rcerd=0;//multichan-options... ignore? 
 		my_mcbsp_confic.xcerc=0;//multichan-options... ignore? 
 		my_mcbsp_confic.xcerd=0;//multichan-options... ignore? 
